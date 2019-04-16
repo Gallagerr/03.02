0a4b84b0b74009cd1c9b8ec72bb4cbfe769b599f
sadiev <sadiev@itstep.kz>
2019-04-16 19:10:15 +0600

    Добавьте файлы проекта.

---
+++ Modul9/Storage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul9
+{
+  public abstract class Storage
+  {
+    public string NameOfDevice { get; set; }
+    public string ModelOfDevice { get; set; }
+    public double FreeSpace { get; set; }
+    public abstract double GetMemory();
+    public abstract string Copying(File[] files);
+    public abstract double GetFreeMemory();
+    public abstract string GetAllInfo();
+  }
+}
